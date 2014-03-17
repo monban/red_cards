16a2921454f6574ba9106b330f1382126776e625
Ian Flynn <himitsu.monban@gmail.com>
2014-03-17 16:14:16 +0000

    just found out about gofmt

--- foo.go
+++ foo.go
@@ -4,36 +4,37 @@ import "encoding/json"
 import "fmt"
 
 type Trinity int
+
 const (
-  Rock Trinity = iota
-  Paper
-  Scissors
+	Rock Trinity = iota
+	Paper
+	Scissors
 )
 
 type leader_card struct {
-  Id int
-  Name string
-  Chrisma int
-  Hp int
-  Ap int
-  Trin Trinity 
+	Id      int
+	Name    string
+	Chrisma int
+	Hp      int
+	Ap      int
+	Trin    Trinity
 }
 
 type support_card struct {
-  id int
-  name string
-  cost int
-  Trin Trinity
+	id   int
+	name string
+	cost int
+	Trin Trinity
 }
 
 type hand struct {
-  leader leader_card
-  support [3]support_card
+	leader  leader_card
+	support [3]support_card
 }
 
 func main() {
-  card := leader_card{1, "Some Leader", 10, 10, 10, Rock}
-  js, err := json.Marshal(card)
-  fmt.Println(err)
-  fmt.Println(string(js))
+	card := leader_card{1, "Some Leader", 10, 10, 10, Rock}
+	js, err := json.Marshal(card)
+	fmt.Println(err)
+	fmt.Println(string(js))
 }
